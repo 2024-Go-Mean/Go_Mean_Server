008d0a31e9085f37fe3cfaccd31d99bc543d3a87
haeunNoh06 <okhagrace06@naver.com>
2024-03-23 23:21:00 +0900

    :sparkles: [feat] advice model 추가

--- models/model.go
+++ models/model.go
@@ -4,12 +4,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Advices struct {
-	Id         primitive.ObjectID
-	CategoryId primitive.ObjectID
-	AiAdvice   string
-}
-
 type Categories struct {
 	Id       primitive.ObjectID
 	Category string
