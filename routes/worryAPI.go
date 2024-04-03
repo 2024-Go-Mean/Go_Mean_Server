70598a6e2aa23344edee3db845b082d7e64f4f52
haeunNoh06 <okhagrace06@naver.com>
2024-04-03 14:51:28 +0900

    :recycle: [refactor] worry get, all get api

--- routes/worryAPI.go
+++ routes/worryAPI.go
@@ -7,7 +7,8 @@ import (
 
 func WorryAPI(router *mux.Router) {
 	router.HandleFunc("/worry", handler.AddWorryHandler).Methods("POST")
-	router.HandleFunc("/worry/{worry_id}", handler.GetWorryHandler).Methods("GET")
+	router.HandleFunc("/worry/{worry_id}", handler.GetOneWorryHandler).Methods("GET")
+	router.HandleFunc("/worry", handler.GetAllWorriesHandler).Methods("GET")
 	router.HandleFunc("/worry/{worry_id}", handler.UpdateWorryHandler).Methods("PATCH")
 	router.HandleFunc("/worry/{worry_id}", handler.DeleteWorryHandler).Methods("DELETE")
 }
