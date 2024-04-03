87a7ddc77f9af2f11834ef6970318c2a7b6f70d1
mic050r <s2216@e-mirim.hs.kr>
2024-04-03 16:39:10 +0900

    ✨ [feat] (#12) 댓글 고민 ID 변경

--- handler/comment.go
+++ handler/comment.go
@@ -3,14 +3,12 @@ package handler
 import (
 	"context"
 	"encoding/json"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"net/http"
-	"strconv"
-
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/http"
 	"noah.io/ark/rest/models"
 )
 
@@ -25,11 +23,18 @@ func initMongoClient() {
 
 func AddCommentHandler(w http.ResponseWriter, r *http.Request) {
 	var comment models.Comment
-	json.NewDecoder(r.Body).Decode(&comment)
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewDecoder(r.Body).Decode(&comment)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	comment.ID = primitive.NewObjectID()
 
 	collection := client.Database("test").Collection("comments")
-	_, err := collection.InsertOne(context.Background(), comment)
+	_, err = collection.InsertOne(context.Background(), comment)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]string{"message": "Failed to add comment"})
@@ -40,29 +45,41 @@ func AddCommentHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetCommentsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// URL에서 게시물 ID를 가져옵니다.
 	params := mux.Vars(r)
-	worryID, _ := strconv.Atoi(params["worry_id"])
+	worryID := params["worry_id"]
 
 	collection := client.Database("test").Collection("comments")
-	cursor, err := collection.Find(context.Background(), bson.M{"worryid": worryID})
+
+	// MongoDB에서 해당 게시물 ID에 해당하는 모든 Comment를 검색합니다.
+	cursor, err := collection.Find(context.Background(), bson.M{"worry_id": worryID})
+
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(map[string]string{"message": "Failed to get comments"})
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer cursor.Close(context.Background())
 
 	var comments []models.Comment
-	defer cursor.Close(context.Background())
+	// 검색된 결과를 Comment 슬라이스에 디코딩합니다.
 	for cursor.Next(context.Background()) {
 		var comment models.Comment
-		cursor.Decode(&comment)
+		err := cursor.Decode(&comment)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		comments = append(comments, comment)
 	}
 
+	// 결과를 JSON 형식으로 반환합니다.
 	json.NewEncoder(w).Encode(comments)
 }
 
 func UpdateCommentHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	commentsID, _ := primitive.ObjectIDFromHex(params["comments_id"])
 
@@ -82,6 +99,7 @@ func UpdateCommentHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteCommentHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	commentsID, _ := primitive.ObjectIDFromHex(params["comments_id"])
 
