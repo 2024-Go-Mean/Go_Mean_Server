91d8a771af3d878fa334ed3a986b72b3b7277d9b
mic050r <s2216@e-mirim.hs.kr>
2024-06-10 17:04:22 +0900

    ✨[feat] CORS (Cross-Origin Resource Sharing) 설정 추가

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 	"noah.io/ark/rest/routes"
@@ -15,7 +16,17 @@ func main() {
 	routes.WorryAPI(router)
 	routes.CategoryAPI(router)
 
+	// CORS 설정
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"}, // 허용할 도메인을 설정
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
+	// CORS 미들웨어로 라우터 감싸기
+	handler := c.Handler(router)
+
 	// 서버 시작
 	fmt.Println("Server started on port 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(":5000", handler))
 }
